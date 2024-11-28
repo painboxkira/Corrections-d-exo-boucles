116555428ec2b314108bf4599bb04b74ef81f8fe
painboxkira <painboxkira@gmail.com>
2024-11-28 21:58:40 +0100

    Delete Exo2.pas

--- Exo2.pas
+++
@@ -1,9 +0,0 @@
-
-program Exo2;
-var i, somme: Integer;
-begin
-    somme := 0;
-    for i := 1 to 100 do
-        somme := somme + i;
-    WriteLn('La somme des entiers de 1 à 100 est : ', somme);
-end.
